2f68dbdeee8e22a8278b1f0fdbe763af587cc934
Ricardo N Feliciano (FelicianoTech) <Ricardo@Feliciano.Tech>
2023-02-02 22:11:33 -0500

    Brew formula update for warden version v0.8.0

--- Formula/warden.rb
+++ Formula/warden.rb
@@ -5,21 +5,21 @@
 class Warden < Formula
   desc "Audit your git repositories based on policy."
   homepage "https://RepoWarden.com"
-  version "0.7.0"
+  version "0.8.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/repowarden/cli/releases/download/v0.7.0/warden-v0.7.0-macos-arm64.tar.gz"
-      sha256 "9ea88f8767a6ab153a1e45c79954e679cf6a25be294942be1ddc5f48e7ef6ab6"
+      url "https://github.com/repowarden/cli/releases/download/v0.8.0/warden-v0.8.0-macos-arm64.tar.gz"
+      sha256 "68e3823b5fcf804c4c1c1e28760dadd71680badda311a136b613b5b7d3eacf67"
 
       def install
         bin.install "warden"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/repowarden/cli/releases/download/v0.7.0/warden-v0.7.0-macos-amd64.tar.gz"
-      sha256 "528fb9a12ee5482261ecbb674f807a4b5d977ad75004cf8ed128671b523d09aa"
+      url "https://github.com/repowarden/cli/releases/download/v0.8.0/warden-v0.8.0-macos-amd64.tar.gz"
+      sha256 "c6bf08e0e2d4659be3362189c6f8b86c1263ca5162ef95b1649f8e3173ad242c"
 
       def install
         bin.install "warden"
@@ -29,16 +29,16 @@ class Warden < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/repowarden/cli/releases/download/v0.7.0/warden-v0.7.0-linux-arm64.tar.gz"
-      sha256 "b97e53dc8c92596cd84d074d4c74a609b1cc0aa7129f7d32d2df4efc3572709b"
+      url "https://github.com/repowarden/cli/releases/download/v0.8.0/warden-v0.8.0-linux-arm64.tar.gz"
+      sha256 "eec10c831bf059f9d72de30f8dba3cdc069b1198130fe5dd43669f4588b1c40a"
 
       def install
         bin.install "warden"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/repowarden/cli/releases/download/v0.7.0/warden-v0.7.0-linux-amd64.tar.gz"
-      sha256 "69440e08dbf55eb058457deba7f61c15cfca4311e66f36db0c61fb0ae2efc4c5"
+      url "https://github.com/repowarden/cli/releases/download/v0.8.0/warden-v0.8.0-linux-amd64.tar.gz"
+      sha256 "4d9e28914b28ba1d95bff6b7074694dbb8bdcd7a3de74dabda17a823480e7282"
 
       def install
         bin.install "warden"
